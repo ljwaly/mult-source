b59838f2dbe3438d3b3490218b7ebf6fe2f75f3b
lijinwu1988 <sh1025462978@sina.com>
2018-11-05 11:24:41 +0800

    增加学号

--- mult-source/src/main/java/com/jw/domain/people/Student.java
+++ mult-source/src/main/java/com/jw/domain/people/Student.java
@@ -30,9 +30,8 @@ public class Student implements Serializable {
 	private String name;
 	private Long age;
 	private String sex;
-
+	private String studentNumber;
 	
-    //@GeneratedValue(strategy = GenerationType.AUTO)
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -72,52 +71,21 @@ public class Student implements Serializable {
 		this.sex = sex;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((age == null) ? 0 : age.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((sex == null) ? 0 : sex.hashCode());
-		result = prime * result + ((studentId == null) ? 0 : studentId.hashCode());
-		return result;
+	
+
+	public String getStudentNumber() {
+		return studentNumber;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Student other = (Student) obj;
-		if (age == null) {
-			if (other.age != null)
-				return false;
-		} else if (!age.equals(other.age))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (sex == null) {
-			if (other.sex != null)
-				return false;
-		} else if (!sex.equals(other.sex))
-			return false;
-		if (studentId == null) {
-			if (other.studentId != null)
-				return false;
-		} else if (!studentId.equals(other.studentId))
-			return false;
-		return true;
+	@Column(name = "student_number")
+	public void setStudentNumber(String studentNumber) {
+		this.studentNumber = studentNumber;
 	}
 
 	@Override
 	public String toString() {
-		return "Student [studentId=" + studentId + ", name=" + name + ", age=" + age + ", sex=" + sex + "]";
+		return "Student [studentId=" + studentId + ", name=" + name + ", age=" + age + ", sex=" + sex
+				+ ", studentNumber=" + studentNumber + "]";
 	}
 
 }
